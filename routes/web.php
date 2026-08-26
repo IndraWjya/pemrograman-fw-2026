17f91f0675d603940f5a1bddedd2b867765b3a67
Indraa <indrawjya31@gmail.com>
2026-08-26 17:33:24 +0700

    Add dashboardController and update routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+use App\Http\Controllers\DashboardController;
+ 
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth'])
+    ->name('dashboard');
+
