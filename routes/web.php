c1b884baf8f133b9f81379e8a107c8c9d1e4998f
Indraa <indrawjya31@gmail.com>
2026-09-02 13:48:24 +0700

    Fix CategorySeeder, add Category model, and update category migration

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@ Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth'])
     ->name('dashboard');
 
+
+Route::get('/about', function () {
+    return "Barokah Mart adalah toko kelontong yang telah melayani pelanggan sejak 2015.
+    Kami berkomitmen menyediakan kebutuhan sehari-hari dengan harga terjangkau dan pelayanan ramah.";
+})->name('about');
